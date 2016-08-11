c14ec400b57def550b298f09d7bebdba5120ce5e
Alejandro Rolón Parodi <ARolonParodi@coasa.com.ar>
2016-08-11 18:20:51 -0300

    agregamos barra de graficos para la encuesta

--- texto.php
+++ texto.php
@@ -1,7 +1,9 @@
-<html>
-<html>
+<!DOCTYPE html>
+<html lang="es">
 <head>
   <title>Consulta</title>
+
+
 </head>
 <body>
   <pre>
@@ -55,5 +57,65 @@
       mysqli_close($conexion);
     ?>
 </pre>
+
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.1/Chart.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.1/Chart.bundle.js"></script>
+
+<div width="400px" height="400px">
+<canvas id="myChart"></canvas></div>
+
+<script>
+var count = <?php echo $count; ?>;
+var promedio1 = <?php echo $promedio1; ?>;
+var promedio2 = <?php echo $promedio2; ?>;
+var promedio3 = <?php echo $promedio3; ?>;
+var promedio4 = <?php echo $promedio4; ?>;
+var promedio5 = <?php echo $promedio5; ?>;
+var promedio6 = <?php echo $promedio6; ?>;
+var promedio7 = <?php echo $promedio7; ?>;
+
+var ctx = document.getElementById("myChart");
+
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Pregunta1", "Pregunta2", "Pregunta3", "Pregunta4", "Pregunta5", "Pregunta6", "Pregunta7"],
+        datasets: [{
+            label: count + " encuestas realizadas!",
+            data: [promedio1,promedio2,promedio3,promedio4,promedio5,promedio6,promedio7],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255,99,132,1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
+
 </body>
 </html>
